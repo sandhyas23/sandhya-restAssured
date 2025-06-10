491d7ba222f02ca9658aec5d2d32d072442b90ab
Sandhya Sankaran <svsandhya23@gmail.com>
2025-06-10 11:31:25 -0700

    created objects for user and address

--- src/test/java/requestObjects/Address.java
+++ src/test/java/requestObjects/Address.java
@@ -1,5 +1,56 @@
 package requestObjects;
 
 public class Address {
+	 private String plotNumber,street, state,country;
+	    int zipCode;
+
+	    public Address(String plotNumber, String street, String state, String country, int zipCode){
+	        this.plotNumber = plotNumber;
+	        this.street = street;
+	        this.state = state;
+	        this.country = country;
+	        this.zipCode = zipCode;
+	    }
+
+	    public String getPlotNumber() {
+	        return plotNumber;
+	    }
+
+	    public void setPlotNumber(String plotNumber) {
+	        this.plotNumber = plotNumber;
+	    }
+
+	    public String getStreet() {
+	        return street;
+	    }
+
+	    public void setStreet(String street) {
+	        this.street = street;
+	    }
+
+	    public String getState() {
+	        return state;
+	    }
+
+	    public void setState(String state) {
+	        this.state = state;
+	    }
+
+	    public String getCountry() {
+	        return country;
+	    }
+
+	    public void setCountry(String country) {
+	        this.country = country;
+	    }
+
+	    public int getZipCode() {
+	        return zipCode;
+	    }
+
+	    public void setZipCode(int zipCode) {
+	        this.zipCode = zipCode;
+	    }
+	
 
 }
--- src/test/java/requestObjects/User.java
+++ src/test/java/requestObjects/User.java
@@ -2,4 +2,57 @@ package requestObjects;
 
 public class User {
 
+	private String userFirstName, userLastName,userEmailId;
+    long userContactNumber;
+    Address userAddress;
+
+    public User(Address userAddress,String userFirstName,String userLastName,long userContactNumber,String userEmailId){
+        this.userAddress = userAddress;
+        this.userFirstName = userFirstName;
+        this.userLastName = userLastName;
+        this.userContactNumber = userContactNumber;
+        this.userEmailId = userEmailId;
+    }
+
+    public Address getUserAddress() {
+        return userAddress;
+    }
+
+    public void setUserAddress(Address userAddress) {
+        this.userAddress = userAddress;
+    }
+
+    public String getUserLastName() {
+		return userLastName;
+	}
+
+	public void setUserLastName(String userLastName) {
+		this.userLastName = userLastName;
+	}
+
+	public String getUserEmailId() {
+		return userEmailId;
+	}
+
+	public void setUserEmailId(String userEmailId) {
+		this.userEmailId = userEmailId;
+	}
+
+	public long getUserContactNumber() {
+		return userContactNumber;
+	}
+
+	public void setUserContactNumber(long userContactNumber) {
+		this.userContactNumber = userContactNumber;
+	}
+
+	public void setUserFirstName(String userFirstName) {
+		this.userFirstName = userFirstName;
+	}
+
+	public String getUserFirstName() {
+        return userFirstName;
+    }
+
+   
 }
