662f178fea6c4e894713b7f00ebc7025de04577f
Sandhya Sankaran <svsandhya23@gmail.com>
2025-06-12 13:28:52 -0700

    added step def for patch

--- src/test/java/stepDefinitions/UserApiStepDef.java
+++ src/test/java/stepDefinitions/UserApiStepDef.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import java.util.Map;
 
+import org.json.JSONObject;
+
 import hooks.Hooks;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
@@ -23,65 +25,71 @@ public class UserApiStepDef extends Utils {
 	private RequestSpecification request;	
 	private Response response;
 	private static int newUserId;
+	private static int mandatoryUserId;
 	private static String newUserFirstName;
+	private static String mandatoryUserFirstName;
 	String currentScenario;
+	String endPoint;
 	private Map<String,List<Map<String,String>>> excel = Hooks.excelData;
 	List<Map<String,String>> requestData;
 	Map<String,String> scenarioRow;
+	private String sheetName = "";
 	//         Get,[ [scenario, statusCode, statusMsg, endPoint],[allUsers,200,Found,/users],
 	//                [validUserId,200,Found,/user{userId}] ]
 	
 	
 	
-	@Given("The User is authorized")
+	@Given("the User is authorized")
     public void the_user_is_authorized() {
         RestAssured.baseURI = ConfigReader.getProperty("baseUrl");
         request = createAuthorizedRequest();
     }
-
-    @Given("the user has valid request header")
-    public void the_user_has_valid_request_header() {
-        request.header("Content-Type", "application/json");
-
-    }
-    
-
-	@When("the user send Post request from {string}")
-	public void the_user_send_post_request_from(String scenario) {
+	
+	
+	@Given("{string} request with BaseURL and valid EndPoint from {string}")
+	public void request_with_base_url_and_valid_end_point_from(String requestName, String scenario) {
 		    currentScenario = scenario;
-		    requestData = excel.get("Post");
+		    sheetName = requestName;
+		    requestData = excel.get(sheetName);
 		    //LoggerLoad.info("requestData: " + requestData);
 		    scenarioRow = getScenarioRow(requestData, scenario);
-		    String endPoint = scenarioRow.get("endPoint");
+		    endPoint = scenarioRow.get("endPoint");
+		    request.header("Content-Type", "application/json");
+	}
 
-		    Address addressObject = buildAddress(scenarioRow, scenario.equals("validMandatory"));
+
+	@When("the user sends Post request")
+	public void the_user_sends_post_request() {
+
+		    Address addressObject = buildAddress(scenarioRow, currentScenario.equals("validMandatory"));
 		    User userObject = buildUser(scenarioRow, addressObject);
 
 		   System.out.println(userObject);
 
 		    response = request.body(userObject).post(endPoint);
 
-		    if (response.statusCode()== 201 && scenario.equals("validDataAll")) {
+		    if (response.statusCode()== 201 && currentScenario.equals("validDataAll")) {
 		    	LoggerLoad.info("User created successfully with ID: " + response.then().extract().path("userId"));
 		        newUserId = response.then().extract().path("userId");
 		        newUserFirstName = response.then().extract().path("userFirstName");
 			}
+		    else if (response.statusCode()== 201 && currentScenario.equals("validMandatory")) {
+		    	LoggerLoad.info("User created successfully with mandatory fields only with ID: " + response.then().extract().path("userId"));
+		        mandatoryUserId = response.then().extract().path("userId");
+		        mandatoryUserFirstName = response.then().extract().path("userFirstName");
+		    }
 	    
 	}
 	
 
 
-	@When("the user send Get request from {string}")
-	public void the_user_send_get_request_from(String scenario) {
-		   currentScenario = scenario;
-		   requestData = excel.get("Get");
-		   scenarioRow = getScenarioRow(requestData, scenario);
-		   String endPoint = scenarioRow.get("endPoint");
+	@When("the user sends Get request")
+	public void the_user_send_get_request() {
 		   
-		   if (scenario.equals("allUsers") || scenario.equals("wrongEndpoint") ||
-			        scenario.equals("invalidUserId") || scenario.equals("invalidUserFirstname")) {
+		   if (currentScenario.equals("allUsers") || currentScenario.equals("wrongEndpoint") ||
+				   currentScenario.equals("invalidUserId") || currentScenario.equals("invalidUserFirstname")) {
 			        response = request.get(endPoint);
-			    } else if (scenario.equals("validUserFirstname")) {
+			    } else if (currentScenario.equals("validUserFirstname")) {
 			        response = request.get(endPoint, newUserFirstName);
 			    } else {
 			        response = request.get(endPoint, newUserId);
@@ -89,19 +97,15 @@ public class UserApiStepDef extends Utils {
 			}
 	
 	
-	@When("the user send Put request from {string}")
-	public void the_user_send_put_request_from(String scenario) {
-		   currentScenario = scenario;
-		   requestData = excel.get("Put");
-		   scenarioRow = getScenarioRow(requestData, scenario);
-		   String endPoint = scenarioRow.get("endPoint");
+	@When("the user sends Put request")
+	public void the_user_send_put_request() {
 		   
-		    Address addressObject = buildAddress(scenarioRow, scenario.equals("validMandatory"));
+		    Address addressObject = buildAddress(scenarioRow, currentScenario.equals("validMandatory"));
 		    User userObject = buildUser(scenarioRow, addressObject);
 
 		   System.out.println(userObject);
 		   
-		   if (scenario.equals("wrongEndpoint") || scenario.equals("invalidUserId")) {
+		   if (currentScenario.equals("wrongEndpoint") || currentScenario.equals("invalidUserId")) {
 			        response = request.body(userObject).put(endPoint);
 			    } 
 			 else {
@@ -111,20 +115,33 @@ public class UserApiStepDef extends Utils {
 			}
 	
 	
-	
-	@When("the user send Delete request from {string}")
-	public void the_user_send_delete_request_from(String scenario) {
-		   currentScenario = scenario;
-		   requestData = excel.get("Delete");
-		   scenarioRow = getScenarioRow(requestData, scenario);
-		   String endPoint = scenarioRow.get("endPoint");
+	@When("the user sends Patch request")
+	public void the_user_send_patch_request() {
+           
+		JSONObject userObj = buildUserForPatch(scenarioRow,currentScenario);
+		LoggerLoad.info(userObj);
 		   
-		   if (scenario.equals("wrongEndpoint") || scenario.equals("invalidUserId")) {
+		   if (currentScenario.equals("wrongEndpoint") || currentScenario.equals("invalidUserId")) {
+			        response = request.body(userObj).patch(endPoint);
+			    } 
+			 else {
+				 //LoggerLoad.info("UserID in patch: " + newUserId+" ,  "+newUserFirstName);
+			        response = request.body(userObj.toString()).patch(endPoint, newUserId);
+			      LoggerLoad.info(response.asString());
+			    }
+			}
+	
+	
+	
+	@When("the user sends Delete request")
+	public void the_user_send_delete_request() {
+		
+		   if (currentScenario.equals("wrongEndpoint") || currentScenario.equals("invalidUserId")) {
 			        response = request.delete(endPoint);
-			    } else if (scenario.equals("validUserFirstname")) {
-			        response = request.delete(endPoint, newUserFirstName);
+			    } else if (currentScenario.equals("deleteValidUserFirstname")) {
+			        response = request.delete(endPoint, mandatoryUserFirstName);
 			    } else {
-			    	LoggerLoad.info("UserID in delete: " + newUserId+" ,  "+newUserFirstName);
+			    	//LoggerLoad.info("UserID in delete: " + newUserId+" ,  "+newUserFirstName);
 			        response = request.delete(endPoint, newUserId);
 			    }
 			}
@@ -135,7 +152,7 @@ public class UserApiStepDef extends Utils {
 	@Then("The user receives status code and valid response")
 	public void the_user_receives_status_code_and_valid_response() {
 		
-		validateResponse(response, scenarioRow);
+		validateResponse(response, scenarioRow, sheetName);
 		
 	}
 
