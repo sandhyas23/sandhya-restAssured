814f736fd4375ce981c60a3b90ca5a2795f6a100
Sandhya Sankaran <svsandhya23@gmail.com>
2025-06-12 13:28:39 -0700

    added object build method for patch

--- src/test/java/utilities/Utils.java
+++ src/test/java/utilities/Utils.java
@@ -5,6 +5,7 @@ import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 import requestObjects.Address;
 import requestObjects.User;
+import org.json.JSONObject;
 
 import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
 import static org.hamcrest.Matchers.equalTo;
@@ -15,23 +16,25 @@ import java.util.Map;
 public class Utils {
 	
 	  
-	  public void validateResponse(Response response, Map<String,String> row) {
+	  public void validateResponse(Response response, Map<String,String> row, String sheetName) {
 		    int expectedStatusCode = (int)Double.parseDouble(row.get("statusCode"));
 		    String expectedStatusLine = row.get("statusLine");
 		    //System.out.println(row);
-		    String schemaFile = "user-schema.json";
+		    String schemaFileForObject = "user-schema.json";
+		    String schemaFileForArray = "user-array-schema.json";
 		   // String userFirstName =
 //		    String expectedValue = row.size() > 6 ? row.get(6) : null;
 //		    String errorJsonPath = row.size() > 7 ? row.get(7) : null;
 //		    String expectedErrorMessage = row.size() > 8 ? row.get(8) : null;
 		    String scenario = row.get("scenario");
 
-		    if (scenario.toLowerCase().contains("invalid") || scenario.toLowerCase().contains("existing")) {
-		        validateNegativeResponse(response, expectedStatusCode, expectedStatusLine);
-		    } else if (scenario.equalsIgnoreCase("allUsers")) {
-		        validateGetAllResponse(response, expectedStatusCode, expectedStatusLine, schemaFile);
-		    } else if (scenario.toLowerCase().startsWith("valid") && schemaFile != null) {
-		        validatePositiveResponse(response, expectedStatusCode, expectedStatusLine, schemaFile);
+		    if (scenario.toLowerCase().contains("invalid") || scenario.toLowerCase().contains("existing") ||
+		    		scenario.toLowerCase().contains("delete")) {
+		            validateNegativeAndDeleteResponse(response, expectedStatusCode, expectedStatusLine);
+		    } else if (scenario.equalsIgnoreCase("allUsers") || scenario.equalsIgnoreCase("validUserFirstname")) {
+		        validateGetAllOrManyResponse(response, expectedStatusCode, expectedStatusLine, schemaFileForArray);
+		    } else if (scenario.toLowerCase().startsWith("valid") && schemaFileForObject != null) {
+		        validatePositiveResponse(response, expectedStatusCode, expectedStatusLine, schemaFileForObject);
 		    } else if(scenario.toLowerCase().contains("wrong")) {
 		    	validateErrorResponse(response,expectedStatusCode, expectedStatusLine);
 		    }
@@ -45,13 +48,13 @@ public class Utils {
 	  public void validatePositiveResponse(Response response, int expectedStatusCode, String expectedStatusLine, String schemaFile) {
 	        response.then()
 	            .statusCode(expectedStatusCode)
-	            .statusLine(expectedStatusLine);
+	            .statusLine(expectedStatusLine)
 	            //.body(jsonPath, equalTo(expectedValue))
-	           // .body(matchesJsonSchemaInClasspath(schemaFile));
+	            .body(matchesJsonSchemaInClasspath(schemaFile));
 	    }
 
 	    // Negative: Validate error status code, status line, and error message
-	    public void validateNegativeResponse(Response response, int expectedStatusCode, String expectedStatusLine) {
+	    public void validateNegativeAndDeleteResponse(Response response, int expectedStatusCode, String expectedStatusLine) {
 	        response.then()
 	            .statusCode(expectedStatusCode)
 	            .statusLine(expectedStatusLine);
@@ -59,7 +62,7 @@ public class Utils {
 	    }
 
 	    // GET all: Validate status code, status line, and schema for list response
-	    public void validateGetAllResponse(Response response, int expectedStatusCode, String expectedStatusLine, String schemaFile) {
+	    public void validateGetAllOrManyResponse(Response response, int expectedStatusCode, String expectedStatusLine, String schemaFile) {
 	        response.then()
 	            .statusCode(expectedStatusCode)
 	            .statusLine(expectedStatusLine)
@@ -119,6 +122,57 @@ public class Utils {
 		public User buildUser(Map<String,String> row, Address address) {
 		    return new User(row.get("userFirstName"), row.get("userLastName"), 
 		    		(long)Double.parseDouble(row.get("userContactNumber")), row.get("userEmailId"), address);
+		}
+		
+		public JSONObject buildUserForPatch(Map<String,String> row, String scenario) {
+			 JSONObject userPatch = new JSONObject();
+			    switch (scenario) {
+			        case "validFirstNamePatch":
+			            userPatch.put("userFirstName", row.get("userFirstName"));
+			            break;
+			        case "validEmailIdPatch":
+			            userPatch.put("userEmailId", row.get("userEmailId"));
+			            break;
+			        case "validPlotPatch":
+			        	JSONObject address1 = new JSONObject();
+			            address1.put("plotNumber", row.get("plotNumber"));
+			            userPatch.put("userAddress", address1);
+			            break;
+			        case "existingContactNo":
+			            userPatch.put("userContactNumber", (long)Double.parseDouble(row.get("userContactNumber")));
+			            break;
+			        case "existingEmailId":
+			         
+			            userPatch.put("userEmailId", row.get("userEmailId"));
+			            break;
+			        case "invalidContactNo":
+			            userPatch.put("userContactNumber", (long)Double.parseDouble(row.get("userContactNumber")));
+			            
+			            break;
+			        case "invalidEmailId":
+			            userPatch.put("userEmailId", row.get("userEmailId"));
+			            
+			            break;
+			        case "invalidPlotNumber":
+			        	JSONObject address2 = new JSONObject();
+			            address2.put("plotNumber", row.get("plotNumber"));
+			            userPatch.put("userAddress", address2);
+			            
+			            break;
+			        case "invalidUserId":
+			            userPatch.put("userFirstName", row.get("userFirstName"));
+			            
+			            break;
+			        case "wrongEndpoint":
+			            userPatch.put("userFirstName", row.get("userFirstName"));
+			            
+			            break;
+			        default:
+			            // Return empty object for unknown scenarios
+			            break;
+			    }
+			    return userPatch;
+			
 		}
 
 
